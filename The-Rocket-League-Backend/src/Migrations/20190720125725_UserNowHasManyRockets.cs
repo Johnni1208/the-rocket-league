bfb8653131ad5dd168154e65802a959163826197
Johnni1208 <developer@johnlouisjacobs.de>
2019-07-20 15:02:20 +0200

    Added Rocket Database

---
+++ The-Rocket-League-Backend/src/Migrations/20190720125725_UserNowHasManyRockets.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace The_Rocket_League_Backend.Migrations
+{
+    public partial class UserNowHasManyRockets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rockets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rockets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rockets_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rockets_UserId",
+                table: "Rockets",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rockets");
+        }
+    }
+}
---
+++ The-Rocket-League-Backend/src/Models/Rocket.cs
@@ -0,0 +1,8 @@
+namespace The_Rocket_League_Backend.Models{
+    public class Rocket{
+        public int Id{ get; set; }
+
+        public User User{ get; set; }
+        public int UserId{ get; set; }
+    }
+}
