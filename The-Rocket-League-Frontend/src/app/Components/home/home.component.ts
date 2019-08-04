5ab9178f4fc5729185e3bd0fa105b26610ae37bc
Johnni1208 <developer@johnlouisjacobs.de>
2019-08-04 20:09:43 +0200

    TRL10: Added rocket flying animation on succesfull http post

--- The-Rocket-League-Frontend/src/app/Components/home/home.component.ts
+++ The-Rocket-League-Frontend/src/app/Components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RocketService} from '../../Services/rocket/rocket.service';
 import {AlertifyService} from '../../Services/alertify/alertify.service';
 
@@ -7,18 +7,36 @@ import {AlertifyService} from '../../Services/alertify/alertify.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  private clicked = 0;
+  private readonly TIMES_IT_NEEDS_TO_BE_CLICKED = 2;
+  isRocketFiring: boolean;
+  isRocketFlying: boolean;
 
-  constructor(private rocketService: RocketService, private alertify: AlertifyService) { }
-
-  ngOnInit() {
+  constructor(private rocketService: RocketService, private alertify: AlertifyService) {
   }
 
   addRocket() {
-    this.rocketService.addRocket().subscribe(() => {
-      this.alertify.success('Rocket getrunken.');
-    }, error => {
-      this.alertify.error('Konnte nicht abheben.');
-    });
+    this.clicked++;
+    this.isRocketFiring = true;
+
+    if (this.clicked === this.TIMES_IT_NEEDS_TO_BE_CLICKED) {
+      this.rocketService.addRocket().subscribe(() => {
+        this.isRocketFlying = true;
+        this.clicked = 0;
+        this.resetAnimation();
+      }, error => {
+        this.alertify.error('Konnte nicht abheben.');
+        this.isRocketFiring = false;
+        this.clicked = 0;
+      });
+    }
+  }
+
+  resetAnimation() {
+    setTimeout(() => {
+      this.isRocketFiring = false;
+      this.isRocketFlying = false;
+    }, 3000);
   }
 }
