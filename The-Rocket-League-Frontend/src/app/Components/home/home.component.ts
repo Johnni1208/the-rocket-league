1e6849b2308d689ea60326b090229d246bc9e80f
Johnni1208 <developer@johnlouisjacobs.de>
2019-08-06 11:35:35 +0200

    TRL11: User now receives an event list of drunken rockets

--- The-Rocket-League-Frontend/src/app/Components/home/home.component.ts
+++ The-Rocket-League-Frontend/src/app/Components/home/home.component.ts
@@ -1,27 +1,41 @@
-import {Component, OnInit} from '@angular/core';
-import {RocketService} from '../../Services/rocket/rocket.service';
-import {AlertifyService} from '../../Services/alertify/alertify.service';
+import { Component, OnInit } from '@angular/core';
+import { RocketService } from '../../Services/rocket/rocket.service';
+import { AlertifyService } from '../../Services/alertify/alertify.service';
+import { Rocket } from '../../Models/rocket';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   private clicked = 0;
   private readonly TIMES_IT_NEEDS_TO_BE_CLICKED = 2;
   public isRocketFiring: boolean;
   public isRocketFlying: boolean;
+  public rocketList: Rocket[];
 
-  constructor(private rocketService: RocketService, private alertify: AlertifyService) {
+  ngOnInit(): void {
+    this.getAllRockets();
   }
 
-  addRocket() {
+  public constructor(
+    private rocketService: RocketService,
+    private alertify: AlertifyService
+  ) { }
+
+  public getTimeAgo(rocket: Rocket): string {
+    return moment(rocket.dateAdded).fromNow();
+  }
+
+  public addRocket() {
     this.clicked++;
     this.isRocketFiring = true;
 
     if (this.clicked === this.TIMES_IT_NEEDS_TO_BE_CLICKED) {
       this.rocketService.addRocket().subscribe(() => {
+        this.getAllRockets();
         this.isRocketFlying = true;
         this.clicked = 0;
         this.resetAnimation();
@@ -33,10 +47,16 @@ export class HomeComponent {
     }
   }
 
-  resetAnimation() {
+  private resetAnimation() {
     setTimeout(() => {
       this.isRocketFiring = false;
       this.isRocketFlying = false;
     }, 3000);
+  }
+
+  private getAllRockets() {
+    this.rocketService.getAllRockets().subscribe(res => {
+      this.rocketList = res.reverse();
+    });
   }
 }
